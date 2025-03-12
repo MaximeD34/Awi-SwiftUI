5a5d20e6e636ef7c874ca1b3a73bde328fed9a4b
Weslie <weslie.rabeson@gmail.com>
2025-03-12 08:58:55 +0100

    init

---
+++ Awi-SwiftUI/Awi_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awi_SwiftUIApp.swift
+//  Awi-SwiftUI
+//
+//  Created by etud on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Awi_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
